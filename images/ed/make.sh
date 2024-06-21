bd26d365dd20ba6fc83e9ec72415ebbd9c96143a
Hossein Esmail <hosteam01@gmail.com>
2024-06-21 23:46:56 +0330

    better logo image

--- images/ed/make.sh
+++ images/ed/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-imagemagick-string -i ':ed' -o foo.png -bg None -fg White -fn 'Ubuntu-Mono' -x 200 -y 200
-imagemagick-trim foo.png
+imagemagick-string -i ':ed' -o foo.png -bg None -fg White -fn 'Ubuntu-Mono' -l 200 -w 200 #-ps 200
+# imagemagick-trim foo.png
 imagemagick-shadow -i foo.png -o foo.png -sc White
